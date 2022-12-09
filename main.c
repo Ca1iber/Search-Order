f6039861390a45ce2a14ec648430ad5bfcdcb8b3
Ca1iber <116082191+Ca1iber@users.noreply.github.com>
2022-12-09 08:53:01 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -4,140 +4,184 @@
 
 
 #define OK          1
-typedef struct list
+typedef struct sort
 {
+    int Sorted;
+    int NonSorted;
     int *data;
-    int length;
-}SSTable;
-typedef struct name
-{
-    char Name[20];
-    int empty;//0为空，1为满
-}Namearr;
+    int Size;
 
-void SQSearch(const SSTable T,int Key);
-void BinSearch(const SSTable T,int Key);
-int Addname(char S[]);
+}EZSort;
+void EasySort(EZSort S);
+void DirectInsert(EZSort S);
+void BubbleSort(EZSort S);
+void QuickSort(int arr[],int lowFlag,int highFlag);
+int Partition(int arr[],int lowf, int highf);
+void Swap(int *a,int *b);
 
 int main(void)
 {
-    char s[20];
-    Namearr arr[30];//存放名字的hash表
-    for(int i=0;i<30;i++)
+    EZSort S;
+    S.Sorted=0;
+    printf("请输入你要排序的个数:\n");
+    scanf("%d",&S.Size);
+    S.NonSorted=S.Size;
+    S.data=(int*)malloc(S.Size*sizeof(int));
+    printf("请输入你要排序的数据:\n");
+    for(int i=0;i<S.Size;i++)
     {
-        arr[i].empty=0;
-        for(int j=0;j<20;j++)
-            arr[i].Name[j]='\0';
+        scanf("%d",&S.data[i]);
     }
-    printf("请输入你要输入的名字个数:\n");
-    int num;
-    scanf("%d",&num);
-    printf("请输入你要输入的名字:\n");
-    for(int i=0;i<num;i++)
-    {
-        scanf("%s",s);
-        int n=Addname(s);
-        int m=n;
-        while(arr[m].empty==1)
-        {
-            m++;
-            m=m%30;
-        }
-        //arr[m].empty==0
-        arr[m].empty=1;//将这格占住
-        strcpy(arr[m].Name,s);//将名字赋给该格
-    }
-    printf("请输入你要查找的名字:\n");
-    char Search[20];
-    scanf("%s",Search);
-    int i;
-    for( i=0;i<30;i++)
-    {
-        int val=strcmp(arr[i].Name,Search);
-        if(val==0)
-            {
-                printf("你要找的名字在第%d位\n",i+1);
-                break;
-            }
 
-    }
-    if(i==30)
-        printf("你要找的名字不在这里面哦......\n");
+    printf("以下是简单排序的排序过程:\n");
+    EasySort(S);
+    printf("\n");
 
-    return 0;
+/*
+    printf("以下是直接插入排序的排序过程:\n");
+    DirectInsert(S);
+    printf("\n");
+*/
+/*
+    printf("以下是冒泡排序的排序过程:\n");
+    BubbleSort(S);
+    printf("\n");
+*/
+/*
+    printf("这是经过快速排序之后的数据:\n");
+    int arr[S.Size];
+    for(int i=0;i<S.Size;i++)
+    {
+        arr[i]=S.data[i];
+    }
+    QuickSort(arr,0,S.Size-1);
+    int x=S.Size;
+    for(int i=0;i<x;i++)
+    {
+        printf("%d  ",arr[i]);
+    }
+*/
 }
 
 
-void SQSearch(const SSTable T,int Key)
+void EasySort(EZSort S)
 {
-    for(int i;i<T.length;i++)
+    int swaped=0;
+    int ar[S.Size];
+    while(S.NonSorted!=0)//当排序尚未完成
     {
-        if(T.data[i]==Key)
+        for(int i=S.Sorted;i<S.Size;i++)//遍历未排序的表
         {
-            printf("找到了!\n你要找的数据%d在第%d个\n",Key,i+1);
-            return OK;
-        }
-    }
-    printf("里面好像没有你要找的%d哦!\n",Key);
-    return OK;
-}
-
-void BinSearch(const SSTable T,int Key)
-{
-        int Low=0;int High=T.length-1;
-        while(Low<=High)
-        {
-            int Mid=(Low+High)/2;
-            if(T.data[Mid]==Key)
+            for(int j=S.Sorted;j<S.Size;j++)//找出未排序子表中最小的元素
             {
-                printf("找到了,你要找的数据%d在第%d个\n",Key,Mid+1);
-                return OK;
-            }
-            else if(Key>T.data[Mid])
-            {
-                Low=Mid+1;
-            }
-            else if(Key<T.data[Mid])
-            {
-                High=Mid-1;
+                if(S.data[j]<S.data[S.Sorted])
+                {
+                    int temp;
+                    temp=S.data[j];
+                    S.data[j]=S.data[S.Sorted];
+                    S.data[S.Sorted]=temp;
+                    swaped=1;
+                }
             }
         }
-        printf("你要找的%d好像不在这里面哦！\n",Key);
-        return OK;
+        if(swaped==0)
+        {
+            printf("已经排好序了，不需要继续了");
+            break;
+        }
+        else
+        {
+            printf("第%d轮排序\n",S.Sorted+1);
+            {
+                for(int i=0;i<S.Size;i++)
+                    printf("%d  ",S.data[i]);
+                    printf("\n");
+            }
+        }
+        swaped=0;
+        S.Sorted++;
+        S.NonSorted--;
+    }
 }
 
-int Addname(char S[])
+void DirectInsert(EZSort S)
 {
-    int sum=0;
-    int n=strlen(S);
-    for(int i=0;i<n;i++)
+    for(int i=0;i<S.Size-1;i++)
     {
-        sum+=(int)S[i];
+        int x=S.data[i+1];
+        int j=i;
+        while((j>-1)&&(x<S.data[i]))
+        {
+            S.data[j+1]=S.data[j];
+            j--;
+        }
+        S.data[j+1]=x;
     }
-    sum=sum%30;
-    return sum;
+    for(int i=0;i<S.Size;i++)
+        printf("%d  ",S.data[i]);
 }
 
+void BubbleSort(EZSort S)
+{
+    int swap=0;
+    for(int i=0;i<S.Size;i++)
+    {
+        for(int j=i+1;j<S.Size;j++)
+        {
+            if(S.data[i]>S.data[j])
+            {
+                int temp=S.data[i];
+                S.data[i]=S.data[j];
+                S.data[j]=temp;
+                swap=1;
+            }
+        }
+        if(swap==1)
+        {
+            printf("这是第%d轮排序之后的数据:\n",i+1);
+             for(int i=0;i<S.Size;i++)
+                printf("%d  ",S.data[i]);
+                printf("\n");
+                swap=0;
+        }
+        else
+        {
+           printf("已经排好序了，不需要继续了");
+            break;
+        }
+    }
+}
 
-    /*
-    SSTable L;
-    L.length=8;
-    for(int i=0;i<L.length;i++)
+int Partition(int arr[],int lowf, int highf)
+{
+    int Key=arr[lowf];
+    while(lowf<highf)
     {
-        L.data=(int*)malloc(L.length*sizeof(int));
+        while(lowf<highf&&arr[highf]>=Key)
+            highf--;
+        Swap(&(arr[lowf]),&arr[highf]);
+        while(lowf<highf&&arr[lowf]<=Key)
+            lowf++;
+        Swap(&(arr[lowf]),&(arr[highf]));
     }
-    printf("请输入你的数据：\n");
-    for(int i=0;i<L.length;i++)
+    return lowf;
+}
+
+void QuickSort(int arr[],int lowFlag,int highFlag)
+{
+    int Keynumber;
+    if(lowFlag<highFlag)
     {
-        scanf("%d",&L.data[i]);
+        Keynumber=Partition(arr,lowFlag,highFlag);
+        QuickSort(arr,lowFlag,Keynumber-1);
+        QuickSort(arr,Keynumber+1,highFlag);
     }
-    printf("请输入你要查找的数据:\n");
-    int Key1,Key2;
-    scanf("%d %d",&Key1,&Key2);
-    printf("以下是查找%d的顺序查找和二分查找:\n",Key1);
-    SQSearch(L,Key1);
-    BinSearch(L,Key1);
-    printf("以下是查找%d的顺序查找和二分查找:\n",Key2);
-    SQSearch(L,Key2);
-    BinSearch(L,Key2);
-    */
+}
+
+void Swap(int *a,int *b)
+{
+    int c;
+    c=*a;
+    *a=*b;
+    *b=c;
+}
